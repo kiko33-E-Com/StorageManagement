7e1b85744ceb60857fb4c94d47f3bb7c8e57f398
kiko33-E-Com <karimjallouli@outlook.com>
2021-02-15 22:59:43 +0100

    My first commit

---
+++ src/test/java/com/gestionnaire/inventaire/InventaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionnaire.inventaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
